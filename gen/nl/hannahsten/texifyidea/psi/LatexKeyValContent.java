b1660d738e1c53c946afb4a63433fbc64edfde61
Thomas Schouten <hollandpirates@gmail.com>
2023-05-27 14:27:42 +0200

    Re-add generated files

---
+++ gen/nl/hannahsten/texifyidea/psi/LatexKeyValContent.java
@@ -0,0 +1,22 @@
+// This is a generated file. Not intended for manual editing.
+package nl.hannahsten.texifyidea.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface LatexKeyValContent extends PsiElement {
+
+  @Nullable
+  LatexCommands getCommands();
+
+  @Nullable
+  LatexMathEnvironment getMathEnvironment();
+
+  @Nullable
+  LatexParameterGroup getParameterGroup();
+
+  @Nullable
+  LatexParameterText getParameterText();
+
+}
